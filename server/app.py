394455c1296ebe2a7d5fe17130c0f86339bfb1d6
BreElle Wells <breellejordyn@gmail.com>
2023-09-15 11:11:43 -0600

    build out routes to handle CRUD actions

--- server/app.py
+++ server/app.py
@@ -5,8 +5,8 @@ from flask_migrate import Migrate
 from models import db, Message
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 CORS(app)
@@ -14,13 +14,48 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+
+@app.route("/messages", methods=["GET", "POST"])
 def messages():
-    return ''
+    if request.method == "GET":
+        messages = Message.query.order_by("created_at").all()
+        response = make_response([m.to_dict() for m in messages], 200)
 
-@app.route('/messages/<int:id>')
+    elif request.method == "POST":
+        data = request.get_json()
+
+        new_message = Message(body=data["body"], username=data["username"])
+
+        db.session.add(new_message)
+        db.session.commit()
+
+        response = make_response(new_message.to_dict(), 201)
+
+    return response
+
+
+@app.route("/messages/<int:id>", methods=["PATCH", "DELETE"])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter(Message.id == id).first()
 
-if __name__ == '__main__':
+    if request.method == "PATCH":
+        data = request.get_json()
+        for attr in data:
+            setattr(message, attr, data[attr])
+
+        db.session.add(message)
+        db.session.commit()
+
+        response = make_response(message.to_dict(), 200)
+
+    elif request.method == "DELETE":
+        db.session.delete(message)
+        db.session.commit()
+
+        response = make_response({"deleted": True}, 200)
+
+    return response
+
+
+if __name__ == "__main__":
     app.run(port=5555)
